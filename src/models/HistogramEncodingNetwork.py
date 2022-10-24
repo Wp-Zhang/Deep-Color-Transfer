6e6227fa42a100f7fd851eada0f044843cba5957
Weipeng Zhang <wp.zhang@hotmail.com>
2022-10-24 16:20:30 -0400

    :construction: Add code of HEN

---
+++ src/models/HistogramEncodingNetwork.py
@@ -0,0 +1,123 @@
+import torch.nn as nn
+from ..util import init_weights
+
+
+def HEN(
+    in_channels: int,
+    out_channels: int,
+    hidden_channels: int = 128,
+    init_method="normal",
+):
+    HEN = HistogramEncodingNetwork(in_channels, out_channels, hidden_channels).cuda()
+    init_weights(HEN, type=init_method)
+
+    return HEN
+
+
+class HistogramEncodingNetwork(nn.Module):
+    """Histogram Encoding Network"""
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        hidden_channels: int = 128,
+    ):
+        """Initialize an HEN
+        Parameters
+        ----------
+        in_channels : int
+            Number of input channels
+        out_channels : int
+            Number of output channels
+        hidden_channels : int, optional
+            Number of hidden channels, by default 128
+        """
+        super(HistogramEncodingNetwork, self).__init__()
+        self.in_channels = in_channels  # 10
+        self.out_channels = out_channels  # 32
+
+        self.conv = nn.Sequential(
+            [
+                nn.Conv2d(
+                    self.in_channels,
+                    hidden_channels,
+                    kernel_size=4,
+                    padding=1,
+                    stride=2,
+                    bias=True,
+                ),
+                nn.LeakyReLU(0.1, True),
+                nn.Conv2d(
+                    hidden_channels,
+                    hidden_channels,
+                    kernel_size=4,
+                    padding=1,
+                    stride=2,
+                    bias=True,
+                ),
+                nn.LeakyReLU(0.1, True),
+                nn.Conv2d(
+                    hidden_channels,
+                    hidden_channels,
+                    kernel_size=4,
+                    padding=1,
+                    stride=2,
+                    bias=True,
+                ),
+                nn.LeakyReLU(0.1, True),
+                nn.Conv2d(
+                    hidden_channels,
+                    hidden_channels,
+                    kernel_size=4,
+                    padding=1,
+                    stride=2,
+                    bias=True,
+                ),
+                nn.LeakyReLU(0.1, True),
+                nn.Conv2d(
+                    hidden_channels,
+                    hidden_channels,
+                    kernel_size=4,
+                    padding=1,
+                    stride=1,
+                    bias=True,
+                ),
+                nn.LeakyReLU(0.1, True),
+                nn.Conv2d(
+                    hidden_channels,
+                    hidden_channels,
+                    kernel_size=4,
+                    padding=1,
+                    stride=1,
+                    bias=True,
+                ),
+                nn.LeakyReLU(0.1, True),
+                nn.Conv2d(
+                    hidden_channels,
+                    hidden_channels,
+                    kernel_size=4,
+                    padding=1,
+                    stride=1,
+                    bias=True,
+                ),
+                nn.LeakyReLU(0.1, True),
+                nn.Conv2d(
+                    hidden_channels,
+                    self.out_channels,
+                    kernel_size=1,
+                    padding=0,
+                    bias=True,
+                ),
+                nn.Flatten(),
+            ]
+        )
+
+        self.fc = nn.Sequential(nn.Linear(out_channels, out_channels))
+
+    def forward(self, input):
+        x = self.conv(input)
+        x = self.fc(x)  # (batch_size, 64)
+        out = x[:, None, None]  # (batch_size, 64, 1, 1)
+
+        return out
