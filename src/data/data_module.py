0d5a4f5ff1e40d4a787427d1ccd8f9c7b0f74ef3
Weipeng Zhang <wp.zhang@hotmail.com>
2022-11-14 12:10:30 -0500

    :construction: Update data preparation code

--- src/data/data_module.py
+++ src/data/data_module.py
@@ -7,15 +7,10 @@ from .dataset import Adobe5kDataset
 
 
 class Adobe5kDataModule(LightningDataModule):
-    def __init__(
-        self, data_dir: str, l_bin: int, ab_bin: int, num_classes: int, batch_size: int
-    ):
+    def __init__(self, data_dir: str, batch_size: int):
         super().__init__()
 
         self.data_dir = Path(data_dir)
-        self.l_bin = l_bin
-        self.ab_bin = ab_bin
-        self.num_classes = num_classes
         self.batch_size = batch_size
 
         self.transform = None
@@ -27,16 +22,12 @@ class Adobe5kDataModule(LightningDataModule):
 
         # Assign train/val datasets for use in dataloaders
         if stage == "fit" or stage is None:
-            trainset = Adobe5kDataset(
-                str(self.data_dir / "train"), self.l_bin, self.ab_bin, self.num_classes
-            )
+            trainset = Adobe5kDataset(str(self.data_dir / "train"))
             self.adb5k_train, self.adb5k_val = random_split(trainset, [0.8, 0.2])
 
         # Assign test dataset for use in dataloader(s)
         if stage == "test" or stage is None:
-            self.adb5k_test = Adobe5kDataset(
-                str(self.data_dir / "test"), self.l_bin, self.ab_bin, self.num_classes
-            )
+            self.adb5k_test = Adobe5kDataset(str(self.data_dir / "test"))
 
     def train_dataloader(self):
         return DataLoader(self.adb5k_train, batch_size=self.batch_size)
